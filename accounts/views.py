9c1982ce93bb1e56ae4edb626c3002186a765c12
UjjwalMrzn <ujjwal.mrzn@gmail.com>
2024-05-02 17:03:15 +0545

    form data to database

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Person
 
 
 
@@ -12,7 +13,24 @@ def home(request):
     return render(request, 'accounts/home.html')
 
 def add(request):
-    return render(request, 'accounts/add.html')
+    if request.method == 'POST':
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        number = request.POST['number']
+        address = request.POST['address']
+        email = request.POST['email']
+        uname = request.POST['uname']
+        # media = request.Post['media']
+
+        form,created=Phone.objects.get_or_create(Number=number)
+        form2,created2=Email.objects.get_or_create(email=email)
+        form3,created2=Address.objects.get_or_create(Address=address)
+        form4,created2=SocialMedia.objects.get_or_create(User_Name=uname)
+
+        new_contact = Person(first_name=fname , last_name=lname ,phone=form, email=form2 , address=form3 , media = form4 )
+        new_contact.save()
+        
+    return render(request, 'accounts/add.html', {})
 
 def search(request):
     query = request.GET['query']
@@ -26,3 +44,5 @@ def delete(request):
     return HttpResponse('delete')
 
 
+# , phone=number , 
+#                              address=address , email=emai
