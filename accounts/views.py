13e6a022e3663765dcad16435b75041053d1aa83
UjjwalMrzn <ujjwal.mrzn@gmail.com>
2024-05-07 12:52:17 +0545

    adddress_seach

--- accounts/views.py
+++ accounts/views.py
@@ -46,7 +46,8 @@ def search(request):
     per2 = Person.objects.filter(first_name__icontains=query)
     per1 = Person.objects.filter(last_name__icontains=query)
     per3 = Person.objects.filter(phone__Number__contains=query)
-    per=per1 or per2 or per3
+    per4 = Person.objects.filter(address__Address__contains=query)
+    per=per1 or per2 or per3 or per4
     # context ={'per' : per} 
     return render(request, 'accounts/search.html' , {'per' : per} )
 
