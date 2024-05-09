f8e9ef1c1752c0ac268e300d42cf5e19164c18d5
UjjwalMrzn <ujjwal.mrzn@gmail.com>
2024-05-09 13:23:17 +0545

    ac creation message

--- accounts/views.py
+++ accounts/views.py
@@ -131,7 +131,7 @@ def register(request):
         if form.is_valid():
             form.save()
             user = form.cleaned_data.get('username')
-            messages.success(request, 'Account was created for ' + user)
+            messages.success(request, 'An account was created for ' + user)
 
             return redirect('login')
 
