b21308ed8be13206e6c8221e23700a02e61e92b1
UjjwalMrzn <ujjwal.mrzn@gmail.com>
2024-05-10 15:42:18 +0545

    redirect home

--- accounts/views.py
+++ accounts/views.py
@@ -138,38 +138,45 @@ def delete(request,pk):
 # user log in and registration
 
 def register(request):
-    form = CreateUserForm()
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
 
-    if request.method == "POST":
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, 'An account was created for ' + user)
+        form = CreateUserForm()
 
-            return redirect('login')
+        if request.method == "POST":
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'An account was created for ' + user)
 
-    context = {'form':form}
-    return render(request , 'accounts/register.html' , context)
+                return redirect('login')
+
+        context = {'form':form}
+        return render(request , 'accounts/register.html' , context)
 
 
 
 def loginpage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else: 
 
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-        user = authenticate(request , username=username , password=password)
+            user = authenticate(request , username=username , password=password)
 
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.info(request, 'Username or Password is incorrect')
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, '"Username or Password is incorrect"')
 
-    context = {}
-    return render(request, 'accounts/loginpage.html' , context)
+        context = {}
+        return render(request, 'accounts/loginpage.html' , context)
 
 
 def logoutuser(request):
