3c075dfb73ca7251237305e0b423cf038dbb786d
UjjwalMrzn <ujjwal.mrzn@gmail.com>
2024-05-24 17:58:59 +0545

    django reset email

--- PhoneBook/settings.py
+++ PhoneBook/settings.py
@@ -139,3 +139,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # settings.py
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+
+
+
+#reset 
+#SMTP Configuration
+# settings.py
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'ujjwal.mrzn@gmail.com'  # Your email address
+EMAIL_HOST_PASSWORD = 'vdvnydgxvldwbkwr'     # Your email password or app password
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('', views.front),
     path('home/', views.home , name='home'),
@@ -16,6 +18,14 @@ urlpatterns = [
     path('register/', views.register , name='register'),
     path('login/', views.loginpage , name='login'),
     path('logout/', views.logoutuser, name='logout'),
+
+
+    #reset
+    path('reset_password/' , auth_views.PasswordResetView.as_view(), name="reset_password"),
+    path('reset_password_sent/' , auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/' , auth_views.PasswordResetConfirmView.as_view() , name="password_reset_confirm"),
+    path('reset_password_complete/' , auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+
 
 
     path('navbar/', views.navbar, name='navbar'),
