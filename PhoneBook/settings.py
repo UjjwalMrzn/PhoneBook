c8d407291df102690ca3e276f5fead24d504b82f
UjjwalMrzn <ujjwal.mrzn@gmail.com>
2024-05-26 19:36:04 +0545

    copyright footer year

--- PhoneBook/settings.py
+++ PhoneBook/settings.py
@@ -153,3 +153,21 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ujjwal.mrzn@gmail.com'  # Your email address
 EMAIL_HOST_PASSWORD = 'vdvnydgxvldwbkwr'     # Your email password or app password
+
+#time/year
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'PhoneBook.context_processors.current_year',  # Add this line
+            ],
+        },
+    },
+]
